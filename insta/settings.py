4603b4cc299d75886e19f370380613fa935ea9b7
ramiz020 <115406632+ramiz020@users.noreply.github.com>
2023-04-16 19:26:16 +0400

    Update settings.py

--- insta/settings.py
+++ insta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sj@3q%aqc9t#n(h=sidkv6w%do285_^rv!3%6+o%&wl4&w(kck'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
